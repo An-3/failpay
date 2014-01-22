b8afa874a7396082712ca724c4150d80ce972245
Vitaliy <skv.gwww@gmail.com>
2014-01-22 11:41:07 +0200

    update trans

--- application/controllers/trans.php
+++ application/controllers/trans.php
@@ -4,10 +4,11 @@ class Trans extends CI_Controller {
 
     function index() {
         $this->load->model('trans_model');
-        if ($this->uri->segment(3) !== FALSE) {
+        if ($this->uri->segment(2) === FALSE) {
             $data['content'] = $this->trans_model->getData();
         }else{
-            $data['content'] = $this->trans_model->getData();
+            $user_id = $this->uri->segment(2);
+            $data['content'] = $this->trans_model->getData($user_id);
         }
         $data['page_title'] = "List of transaction";
         $this->load->view('trans_view', $data);
@@ -15,9 +16,26 @@ class Trans extends CI_Controller {
 
     function transaction() {
         $data['page_title'] = "Save transaction";
+        $this->load->model('trans_model');
         $user_id = $this->uri->segment(2);
+        $data = array();
         $amount_money = ($this->uri->segment(3) !== FALSE) ? $this->uri->segment(3) : 0;
-        $this->load->view('trans_view', $data);
+        if( $this->trans_model->getUser($user_id) ){
+            if( $_SERVER['REMOTE_ADDR'] == '172.16.5.177' ){ 
+                $data['user_id'] = $user_id;
+                $data['amount_money'] = $amount_money;
+                $data['amount_time'] = 0;
+                $data['late_date'] = date('Y-m-d H:i:s');
+                $data['type_id'] = 1;
+                $data['tariff_id'] = 1;
+                $data['content'] = $this->trans_model->saveData($data);
+            }else{
+               $data['content'] = 'You dont have access'; 
+            }
+        }else{
+            $data['content'] = 'No such user'; 
+        }
+        $this->load->view('transSave_view', $data);
     }
     
 
--- application/models/trans_model.php
+++ application/models/trans_model.php
@@ -1,19 +1,49 @@
-<?php   
-class Trans_model extends CI_Model {   
- 
-    function Failpay_model()   
-    {   
+<?php
+
+class Trans_model extends CI_Model {
+
+    function Failpay_model() {
         // Вызов конструктора  
-        parent::CI_Model();   
-    }   
- 
-    function getData()   
-        {   
-            //Выбираем все данные из таблицы tdata  
-            $query = $this->db->get('transactions'); 
-            //Возвращаем результат   
-            return $query->result();      
-        }   
- 
-}   
+        parent::CI_Model();
+    }
+
+    function getData($u_id = false) {
+        if (!$u_id) {
+            $query = $this->db->get('transactions');
+        } else {
+            $query = $this->db->query("SELECT * FROM `transactions` WHERE `user_id` = '{$u_id}'");
+        }
+        return $query->result();
+    }
+
+    function saveData(array $data) {
+        if (!empty($data)) {
+            try {
+                if (!$this->db->insert('transactions', $data))
+                    throw new Exception;
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+            return "Thank you!";
+        }
+        else
+            return "Fail!";
+    }
+
+    function getUser($user_id) {
+        if (!empty($user_id)) {
+            try {
+                $query = $this->db->query("SELECT * FROM `users` WHERE `id` = '{$user_id}' LIMIT 1");
+                if (!$query) throw new Exception;
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+        } else {
+            return false;
+        }
+        return $query->row();
+    }
+
+}
+
 ?>
